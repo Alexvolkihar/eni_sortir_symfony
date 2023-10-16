00483bea17264dead4d232a506f6f65e8ec541c5
am-yl <amyl@caramail.com>
2023-10-16 16:53:44 +0200

    Auto generating sites & states

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Site;
+use App\Entity\State;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,29 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->addSites($manager);
+        $this->addStates($manager);
+    }
 
+    public function addSites(ObjectManager $manager)
+    {
+        $sitesNames = ["Chartres de Bretange", "Saint-Herblain", "La-Roche-sur-Yon", "Quimper", "Niort", "Angers"];
+        foreach ($sitesNames as $name) {
+            $site  = new Site();
+            $site->setName($name);
+            $manager->persist($site);
+        }
+        $manager->flush();
+    }
+
+    public function addStates(ObjectManager $manager)
+    {
+        $statesLabels = ["Créée", "Ouverte", "Clôturée", "Activité en cours", "Passée", "Annulée"];
+        foreach ($statesLabels as $label) {
+            $state  = new State();
+            $state->setLabel($label);
+            $manager->persist($state);
+        }
         $manager->flush();
     }
 }
